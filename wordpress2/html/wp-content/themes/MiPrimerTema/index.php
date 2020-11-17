612e28e67a66a97c8660023d8b1f352ae23b6ac6
Rubén Carrera Amoedo <rcarreraamoedo@danielcastelao.org>
2020-11-17 19:52:43 +0100

    El "loop"

--- wordpress2/html/wp-content/themes/MiPrimerTema/index.php
+++ wordpress2/html/wp-content/themes/MiPrimerTema/index.php
@@ -11,32 +11,37 @@
 <h1><?php bloginfo( 'name' ); ?></h1>
 <h2><?php bloginfo( 'description' ); ?></h2>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php
 
-    <h3><?php the_title(); ?></h3>
 
-    <?php the_content(); ?>
-    <?php wp_link_pages(); ?>
-    <?php edit_post_link(); ?>
+if ( have_posts() ) :
+    while ( have_posts() ) :
+        the_post();
+        the_title('<h3> Titulo:', '</h3>');
+        ?>
+        <h1> <?php the_excerpt(); ?> </h1>
+<?php
+        the_content();
+        wp_link_pages();
+        edit_post_link();
+    endwhile;
 
-<?php endwhile; ?>
 
-    <?php
     if ( get_next_posts_link() ) {
         next_posts_link();
-    }
-    ?>
-    <?php
+    };
+
     if ( get_previous_posts_link() ) {
         previous_posts_link();
-    }
-    ?>
+    };
 
-<?php else: ?>
+else:
+    echo "<p>No posts found. :(</p>";
+endif;
 
-    <p>No posts found. :(</p>
+?>
 
-<?php endif; ?>
+
 <?php get_footer(); ?> <!--Añadido el pie de pagina-->
 <?php wp_footer(); ?>
 </body>
